d3d286b0e050fa6ed882816a7dee4fa03b5a8a38
austinvoice <brian.goffman@gmail.com>
2019-11-01 13:52:44 -0700

    Add hint feature

--- src/NumberGuess.java
+++ src/NumberGuess.java
@@ -1,25 +1,48 @@
 // program to guess a number
 // gives user hints
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuess {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int number = 42; // always pick same number
+        Random rand = new Random(); // init random
+        int number = rand.nextInt(100); // random 0-99
 
-        // user input, fence post
-        System.out.print("Your guess? ");
+        // first user input, fence post
+        System.out.print("Your guess (0-99)? ");
         int guess = console.nextInt();
         int numGuesses = 1;
 
+        // give hints until correct
         while (guess != number) {
-            System.out.println("Incorrect.");
-            System.out.print("Guess again. ");
+            int numMatches = matches(number, guess);
+            System.out.println("Incorrect. Hint: " +
+                    numMatches + " digits match.");
+            System.out.print("Guess again (0-99)? ");
             guess = console.nextInt();
             numGuesses++;
         }
+
         System.out.println("You got it right in " +
                 numGuesses + " tries.");
+    }
+
+    public static int matches(int number, int guess) {
+        int numMatches = 0;
+
+        // test first digit or second digit
+        if (guess / 10 == number / 10 ||
+            guess / 10 == number % 10 ) {
+            numMatches++;
+        }
+        // check second digit for match and case where digits are the same
+        if (guess / 10 != guess % 10 &&
+                (guess % 10 == guess / 10 ||
+                guess % 10 == number % 10)) {
+            numMatches++;
+        }
+        return numMatches;
     }
 }
